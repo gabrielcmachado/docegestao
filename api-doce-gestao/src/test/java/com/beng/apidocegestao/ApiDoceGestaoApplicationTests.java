e3e59b35e83e23faf3833c73e0a015bb1c445468
Gabriel Machado <gabriel.machado.carvalho@gmail.com>
2022-06-30 10:36:58 -0300

    Initial commit

---
+++ api-doce-gestao/src/test/java/com/beng/apidocegestao/ApiDoceGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beng.apidocegestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDoceGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
